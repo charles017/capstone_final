7a6292f96940afc75d3db0425a8754a2e6e9a610
charles017 <charlie.abuloc@myyahoo.com>
2023-10-01 03:25:56 +0800

    post

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ExampleController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -14,13 +14,14 @@ use App\Http\Controllers\UserController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//User related routes
 Route::get('/', [UserController::class, "showCorrectHomepage"]);
-
-Route::get('/about', [ExampleController::class, "aboutPage"]);
-
 Route::post('/register', [UserController::class, "register"]);
-
 Route::post('/login', [UserController::class, "login"]);
+Route::post('/logout', [UserController::class, "logout"]);
+
+//Blog post related routes
+Route::get('/create-post', [PostController::class, "showCreateForm"] );
+Route::post('/create-post', [PostController::class, "storeNewPost"] );
 
 
