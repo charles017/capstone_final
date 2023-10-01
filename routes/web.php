1ee1d2667540f83349d1cadf8369a4cc0ca7f09a
charles017 <charlie.abuloc@myyahoo.com>
2023-10-01 16:35:44 +0800

    major updates

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,29 @@ use App\Http\Controllers\UserController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/admins-only', function() {
+  return 'Only admins should be able to see this page!';
+})->middleware('can:visitAdminPages');
+
 //User related routes
 Route::get('/', [UserController::class, "showCorrectHomepage"])->name('login');
-Route::post('/register', [UserController::class, "register"]);
-Route::post('/login', [UserController::class, "login"]);
-Route::post('/logout', [UserController::class, "logout"]);
+Route::post('/register', [UserController::class, "register"])->middleware('guest');
+Route::post('/login', [UserController::class, "login"])->middleware('guest');
+Route::post('/logout', [UserController::class, "logout"])->middleware('mustBeLoggedIn');
+Route::get('/manage-avatar', [UserController::class, 'showAvatarForm']);
+Route::post('/manage-avatar', [UserController::class, 'storeAvatar']);
+
 
 //Blog post related routes
-Route::get('/create-post', [PostController::class, "showCreateForm"])->middleware('auth');
-Route::post('/create-post', [PostController::class, "storeNewPost"] );
-Route::get('/post/{post}', [PostController::class, "viewSinglePost"] );
+Route::get('/create-post', [PostController::class, "showCreateForm"])->middleware('mustBeLoggedIn');
+Route::post('/create-post', [PostController::class, "storeNewPost"])->middleware('mustBeLoggedIn');
+Route::get('/post/{post}', [PostController::class, "viewSinglePost"]);
+Route::delete('/post/{post}', [PostController::class, 'delete'])->middleware('can:delete,post');
+Route::get('/post/{post}/edit', [PostController::class, 'showEditForm'])->middleware('can:update,post');
+Route::put('/post/{post}', [PostController::class, 'actuallyUpdate'])->middleware('can:update,post');
+
+//Profile related routes
+Route::get('/profile/{user:username}', [UserController::class, "profile"]);
 
 
