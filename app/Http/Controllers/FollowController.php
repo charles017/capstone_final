b1f75a40b211cbd631e5e73b2cec14476e37fcbe
charles017 <charlie.abuloc@myyahoo.com>
2023-10-09 10:38:02 +0800

    follow

---
+++ app/Http/Controllers/FollowController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Follow;
+use Illuminate\Http\Request;
+
+class FollowController extends Controller
+{
+    public function createFollow(User $user) {
+
+        // you cannot follow yourself
+        // you cannot follow someone you're already following
+
+        $newFollow = new Follow;
+        $newFollow->user_id = auth()->user()->id;
+        $newFollow->followeduser = $user->id;
+        $newFollow->save();
+    }
+
+    public function removeFollow() {
+
+    }
+}
